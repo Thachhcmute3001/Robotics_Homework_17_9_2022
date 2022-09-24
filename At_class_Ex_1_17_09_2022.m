a9b14fd6a8914dd1ff8dbd29231b7855dd5f56b6
Thachhcmute3001 <92521250+Thachhcmute3001@users.noreply.github.com>
2022-09-24 13:51:22 +0700

    Add files via upload

---
+++ At_class_Ex_1_17_09_2022.m
@@ -0,0 +1,26 @@
+xA = 60; yA = 30; xB = 20; yB = 60;
+l1 = 50; l2 = 40;
+for t = 0:0.01:1
+    x = xA + (xB - xA)*t;
+    y = yA + (yB - yA)*t;
+    c2 = (Px^2 + Py^2 -l1^2 - l2^2)/(2*l1*l2);
+    s2 = sqrt(abs(1 - c2^2));
+    t2 = atan2(s2,c2);
+    c1 = x*(l1 + l2*s2) + x*l2*s2;
+    s1 = y*(l1 + l2*s2) - y*l2*s2;
+    t1 = atan2(s1,c1);
+    Px = l1*cos(t1) + l2*cos(t1 + t2);
+    Py = l1*sin(t1) + l2*sin(t1 + t2);
+    subplot(2,1,1);
+    plot(t,t1*(180/pi),'.',t,t2*(180/pi),'.');
+    legend('t1','t2');
+    hold on
+    grid on
+    subplot(2,1,2);
+    plot(Px,Py,'.'); xlabel('Px'); ylabel('Py');
+    hold on
+    grid on
+end
+    
+    
+    
---
+++ At_class_Ex_2_17_09_2022.m
@@ -0,0 +1,24 @@
+l1 = 50; l3 = 20; v = 10; b = 10; h = 100;
+for t = 0:0.1: 10
+    t1 = atan2(h - b - l3, v*t);
+    t2 = pi/2 - t1;
+    l2 = sqrt((h - b -l3)^2 + (v*t)^2) - l1;
+    Px = (l1 + l2)*cos(t1) + l3*cos(t1 + t2);
+    Py = (l1 + l2)*sin(t1) + l3*sin(t1 + t2);
+    subplot(2,2,1);plot(t,t1,'.');xlabel('time(s)'); ylabel('t1');
+    hold on
+    grid on
+    subplot(2,2,2); 
+    plot(t,t2,'.');xlabel('time(s)'); ylabel('t2');
+    hold on
+    grid on
+    subplot(2,2,3); 
+    plot(t,l2,'.');xlabel('time(s)'); ylabel('l2');
+    hold on
+    grid on
+    subplot(2,2,4); 
+    plot(Px,Py,'.');xlabel('Px');ylabel('Py');
+    hold on
+    grid on
+    
+end
